4ece31346182945ddad50419d09e439e3c7d32ce
Ryan Croke <ryancroke@Ryans-MacBook-Air.local>
2023-04-19 16:33:39 +0300

    move app in if statement

--- hebrew-st.py
+++ hebrew-st.py
@@ -37,41 +37,37 @@ def check_password():
         return True
 
 if check_password():
-    st.write("Here goes your normal Streamlit app...")
-    st.button("Click me")
+    # Function to get pronunciation and translation from OpenAI API
+    def get_pronunciation_and_translation(text, gender):
+        prompt = f"""
+                Assuming a speaker is {gender} speaking, translate the English phrase `{text}` into Hebrew. Provide the pronunciation as well.  I want you to respond with the format: \n `Hebrew: \n Pronunciation:`
+                """
+        
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=0.3,
+        )
+
+        response_text = response.choices[0].text.strip()
+        print(response_text)
+        translation, pronunciation  = response_text.split("\n")
+
+        return translation, pronunciation
+
+    # Streamlit app
+    st.title("English to Hebrew Translation With Pronunciation")
+
+    # get gender from user
+    gender = st.selectbox("Choose an option:", ['male','female'])
 
 
-# Function to get pronunciation and translation from OpenAI API
-def get_pronunciation_and_translation(text, gender):
-    prompt = f"""
-               Assuming a speaker is {gender} speaking, translate the English phrase `{text}` into Hebrew. Provide the pronunciation as well.  I want you to respond with the format: \n `Hebrew: \n Pronunciation:`
-               """
-    
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.3,
-    )
+    user_input = st.text_input("Enter the English phrase you want to translate:")
 
-    response_text = response.choices[0].text.strip()
-    print(response_text)
-    translation, pronunciation  = response_text.split("\n")
-
-    return translation, pronunciation
-
-# Streamlit app
-st.title("English to Hebrew Translation With Pronunciation")
-
-# get gender from user
-gender = st.selectbox("Choose an option:", ['male','female'])
-
-
-user_input = st.text_input("Enter the English phrase you want to translate:")
-
-if st.button("Submit"):
-    translation, pronunciation = get_pronunciation_and_translation(user_input, gender)
-    st.write("---\n## ",pronunciation)
-    st.write("## ",translation)
+    if st.button("Submit"):
+        translation, pronunciation = get_pronunciation_and_translation(user_input, gender)
+        st.write("---\n## ",pronunciation)
+        st.write("## ",translation)
